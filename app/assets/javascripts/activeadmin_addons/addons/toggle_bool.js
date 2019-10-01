621a0074a0db50719a15466f01c2ff276c260a8a
Atul Shukla <atul.shukla8957@gmail.com>
2019-10-01 12:04:30 +0530

    update for toggle and confirmation message

--- app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
+++ app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
@@ -1,46 +1,53 @@
 var initializer = function() {
   $('.toggle-bool-switch').click(function(e) {
-    var className = $('.toggle-bool-switch').attr('class');
-    let activeClass = className.split(' ')[1]
-    if(activeClass == 'on'){
-    }
+    var prevValue = $(this).hasClass('on');
+    if (prevValue) {
+    } 
     else{
       var boolSwitch = $(e.target);
-      var objectId = boolSwitch.data('object_id');
-      var model = boolSwitch.data('model');
-      var field = boolSwitch.data('field');
+      var retVal = confirm("Are you sure you want to manual update?");
+      if(retVal){
+        var objectId = boolSwitch.data('object_id');
+        var model = boolSwitch.data('model');
+        var field = boolSwitch.data('field');
 
-      var url = boolSwitch.data('url');
-      var value = boolSwitch.data('value');
-      var successMessage = boolSwitch.data('success_message');
-      var data = { id: objectId };
-      data[model] = {};
-      data[model][field] = !value;
-      var retVal = confirm("Do you want to continue ?");
-      if(retVal == true){
-        $.ajax({
-          url: url,
-          data: data,
-          dataType: 'json',
-          headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-          error: function() {
-            var errorMsg = 'Error: Update Unsuccessful';
-            alert(errorMsg);
-          },
-          success: function() {
-            boolSwitch.data('value', !value);
-            boolSwitch.toggleClass('on');
-            if (!boolSwitch.hasClass('notify-success')) return;
-            $(function() {
-              setTimeout(alert(successMessage), 500);
-            });
-          },
-          type: 'PATCH',
-        });
+        var url = boolSwitch.data('url');
+        var value = boolSwitch.data('value');
+        var successMessage = boolSwitch.data('success_message');
+        var data = { id: objectId };
+        data[model] = {};
+        data[model][field] = !value;
+        var resVal = confirm("This changes can't be undone!");
+        if (resVal) {
+          $.ajax({
+            url: url,
+            data: data,
+            dataType: 'json',
+            headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            error: function() {
+              var errorMsg = 'Error: Update Unsuccessful';
+              alert(errorMsg);
+            },
+            success: function() {
+              boolSwitch.data('value', !value);
+              boolSwitch.addClass('on');
+              if (!boolSwitch.hasClass('notify-success')) return;
+              $(function() {
+                setTimeout(alert(successMessage), 500);
+              });
+            },
+            type: 'PATCH',
+          });
+        }else{
+
+        }
+      }
+      else{
       }  
     }  
   });
 };
+
 
 $(initializer);
 $(document).on('turbolinks:load', initializer);
