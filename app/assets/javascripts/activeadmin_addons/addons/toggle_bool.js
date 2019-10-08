f2738f139515b69d739a71e35880dc07605f4c87
Atul Shukla <atul.shukla8957@gmail.com>
2019-10-09 01:16:30 +0530

    change for admin_user

--- app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
+++ app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
@@ -11,58 +11,84 @@ var initializer = function() {
     data[model] = {};
     data[model][field] = !value;
     if (model == "user"){
-      var prevValue = $(this).hasClass('on');
-      if (prevValue) {
-      }else{
-        var retVal = confirm("Are you sure you want to manual update?");
-        if(retVal){
-          var resVal = confirm("This changes can't be undone!");
-          if (resVal) {
-            $.ajax({
-              url: url,
-              data: data,
-              dataType: 'json',
-              headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-              error: function() {
-                var errorMsg = 'Error: Update Unsuccessful';
-                alert(errorMsg);
-              },
-              success: function() {
-                boolSwitch.data('value', !value);
-                boolSwitch.addClass('on');
-                if (!boolSwitch.hasClass('notify-success')) return;
-                $(function() {
-                  setTimeout(alert(successMessage), 500);
-                });
-              },
-              type: 'PATCH',
-            });
+      if (field == "elemental_docket_upload_status"){
+        var prevValue = $(this).hasClass('on');
+        if (prevValue) {
+        }else{
+          var retVal = confirm("Are you sure you want to manually approve the customer?");
+          if(retVal){
+            var resVal = confirm("his function cannot be undone. Are you sure?");
+            if (resVal) {
+              update_manual_status(data, value, url);
+            }else{}
+          }else{} 
+        }
+      }else if(field == "blocked"){
+        var prevValue = $(this).hasClass('on');
+        if (prevValue) {
+          var retVal = confirm("Are you sure you want to unblock this user?");
+          if(retVal){
+            default_toggel_status(data, value, url, boolSwitch);
           }else{}
-        }else{} 
-      }
+        }else{
+          var retVal = confirm("A Blocked user cannot access his/her dashboard. Are you sure you want to block this user?");
+          if(retVal){
+            default_toggel_status(data, value, url, boolSwitch);
+          }else{} 
+        }
+      }     
     }else{
-      $.ajax({
-        url: url,
-        data: data,
-        dataType: 'json',
-        headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-        error: function() {
-          var errorMsg = 'Error: Update Unsuccessful';
-          alert(errorMsg);
-        },
-        success: function() {
-          boolSwitch.data('value', !value);
-          boolSwitch.toggleClass('on');
-          if (!boolSwitch.hasClass('notify-success')) return;
-          $(function() {
-            setTimeout(alert(successMessage), 500);
-          });
-        },
-        type: 'PATCH',
-      });
+      default_toggel_status(data, value, url, boolSwitch);
     }  
   }); 
 };
 
 $(initializer);
 $(document).on('turbolinks:load', initializer);
+
+
+update_manual_status = function(data, value, url, boolSwitch){
+  $.ajax({
+    url: url,
+    data: data,
+    dataType: 'json',
+    headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+    error: function() {
+      var errorMsg = 'Error: Update Unsuccessful';
+      alert(errorMsg);
+    },
+    success: function() {
+      boolSwitch.data('value', !value);
+      boolSwitch.addClass('on');
+      if (!boolSwitch.hasClass('notify-success')) return;
+      $(function() {
+        setTimeout(alert(successMessage), 500);
+      });
+    },
+    type: 'PATCH',
+  });
+}
+
+
+default_toggel_status = function(data, value, url, boolSwitch){
+  $.ajax({
+    url: url,
+    data: data,
+    dataType: 'json',
+    headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+    error: function() {
+      var errorMsg = 'Error: Update Unsuccessful';
+      alert(errorMsg);
+    },
+    success: function() {
+      boolSwitch.data('value', !value);
+      boolSwitch.toggleClass('on');
+      if (!boolSwitch.hasClass('notify-success')) return;
+      $(function() {
+        setTimeout(alert(successMessage), 500);
+      });
+    },
+    type: 'PATCH',
+  });
+}
+
